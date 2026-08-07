40361fae2b9d5e87d7aca14127586b172675129c
Ganesh-Chougale <gchougale32@gmail.com>
2026-08-07 16:34:25 +0530

    saved

--- Personal/Engineering/Krypt/Programm/javascript/sub_scripts/Mapper.js
+++ Personal/Engineering/Krypt/Programm/javascript/sub_scripts/Mapper.js
@@ -1,76 +1,61 @@
 const mapper = {
-    asciiToVismur: {},
+  asciiToVismur: {},
 
-    vismurToAscii: {},
+  vismurToAscii: {},
 
-    initialize() {
-        for (let i = 0; i < 128; i++) {
-            this.asciiToVismur[harcodedAsciiChars[i]] = vismur[i];
+  initialize() {
+    for (let i = 0; i < 128; i++) {
+      this.asciiToVismur[harcodedAsciiChars[i]] = vismur[i];
 
-            this.vismurToAscii[vismur[i]] = harcodedAsciiChars[i];
-        }
-    },
+      this.vismurToAscii[vismur[i]] = harcodedAsciiChars[i];
+    }
+  },
 
-    encrypt(text) {
-        let result = "";
-        let unknown = [];
+  encrypt(text) {
+    let result = "";
+    let unknown = [];
 
-        for (const char of text) {
-            if (this.asciiToVismur[char]) {
-                result += this.asciiToVismur[char];
-            } else {
-                result += char;
+    for (const char of text) {
+      if (this.asciiToVismur[char]) {
+        result += this.asciiToVismur[char];
+      } else {
+        result += char;
 
-                unknown.push(char);
-            }
-        }
+        unknown.push(char);
+      }
+    }
 
-        return {
-            result,
-            unknown,
-        };
-    },
+    return {
+      result,
+      unknown,
+    };
+  },
 
-    decrypt(text) {
-        let result = "";
-        let unknown = [];
+  decrypt(text) {
+    let result = "";
+    let unknown = [];
+    const segmenter = new Intl.Segmenter("en", { granularity: "grapheme" });
 
-        for (const char of text) {
-            if (this.vismurToAscii[char]) {
-                result += this.vismurToAscii[char];
-            } else {
-                result += char;
+    for (const { segment } of segmenter.segment(text)) {
+      if (this.vismurToAscii[segment]) {
+        result += this.vismurToAscii[segment];
+      } else {
+        result += segment;
+        unknown.push(segment);
+      }
+    }
 
-                unknown.push(char);
-            }
-        }
-
-        return {
-            result,
-            unknown,
-        };
-    },
-loadCustomMapping(mapping){
-
-
+    return { result, unknown };
+  },
+  loadCustomMapping(mapping) {
     this.asciiToVismur = {};
 
     this.vismurToAscii = {};
 
+    for (const key in mapping) {
+      this.asciiToVismur[key] = mapping[key];
 
-    for(const key in mapping){
-
-        this.asciiToVismur[key]
-            =
-        mapping[key];
-
-
-        this.vismurToAscii[mapping[key]]
-            =
-        key;
-
+      this.vismurToAscii[mapping[key]] = key;
     }
-
-}
-
+  },
 };
